cbe4db9f6736466aad601ced8cf1f47ac1109f4d
half-anerd <74734120+half-anerd@users.noreply.github.com>
2020-11-27 10:46:51 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,10 @@
+x = float(input("Enter value of x:"))
+y = float(input("Enter value of y:"))
+z = float(input("Enter value of z:"))
+a = 0
+while a*x < y:
+  a += 1
+if y <= a*x <= y+z:
+  print("YES")
+else:
+  print("NO") 
