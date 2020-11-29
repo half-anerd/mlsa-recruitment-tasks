53a95ebfba51b3c0f5605041d433e88e36cb0fa2
half-anerd <74734120+half-anerd@users.noreply.github.com>
2020-11-29 11:09:30 +0530

    Update task0.py

--- task0.py
+++ task0.py
@@ -8,15 +8,19 @@ k = 0
 p = 0
 for i in range(n):
     if c[i] != 0:
-      m = i  
+      m = i
       k = c[i]
-      for i in range(m,n):
-         if c[i] - k != 0:
-           l = i
-           for i in range(l,n):
-             if c[i] != 0:
-                 p += 1
-                 break
+      if k < 0:
+         p += 1
+         break
+      else:
+         for i in range(m,n):
+             if c[i] - k != 0:
+                l = i
+                for i in range(l,n):
+                    if c[i] != 0:
+                       p += 1
+                       break
 
 if p == 0:
     print("YES")
